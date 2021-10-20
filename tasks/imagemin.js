597bfa2c5e3c7e130f9005d0797a07b4d44781d7
Gabriel Paul 'Cley Faye' Risterucci <cleyfaye@gmail.com>
2021-10-20 12:40:21 +0200

    Updated code to follow dependencies update

--- tasks/imagemin.js
+++ tasks/imagemin.js
@@ -1,29 +1,31 @@
 const os = require('os');
 const chalk = require('chalk');
-const imagemin = require('imagemin');
 const plur = require('plur');
 const prettyBytes = require('pretty-bytes');
-const pMap = require('p-map');
 
-const defaultPlugins = ['gifsicle', 'jpegtran', 'optipng', 'svgo'];
+const defaultPlugins = ['gifsicle', 'jpegtran', 'optipng', 'svgo/index.js'];
 
-const loadPlugin = (grunt, plugin, opts) => {
+const loadPlugin = async (grunt, plugin, options) => {
 	try {
-		return require(`imagemin-${plugin}`)(opts);
-	} catch (error) {
+		/* eslint-disable node/no-unsupported-features/es-syntax */
+		const module = await import(`imagemin-${plugin}`);
+		if ('default' in module) {
+			return module.default(options);
+		}
+
+		return module(options);
+		/* eslint-enable node/no-unsupported-features/es-syntax */
+	} catch {
 		grunt.warn(`Couldn't load default plugin "${plugin}"`);
 	}
 };
 
-const getDefaultPlugins = (grunt, opts) => defaultPlugins.reduce((plugins, plugin) => {
-	const instance = loadPlugin(grunt, plugin, opts);
-
-	if (!instance) {
-		return plugins;
-	}
-
-	return plugins.concat(instance);
-}, []);
+const getDefaultPlugins = async (grunt, options) => {
+	const loadingPromises = defaultPlugins.map(plugin => loadPlugin(grunt, plugin, options));
+	const instances = await Promise.all(loadingPromises);
+	const plugins = instances.filter(instance => Boolean(instance));
+	return plugins;
+};
 
 module.exports = grunt => {
 	grunt.registerMultiTask('imagemin', 'Minify PNG, JPEG, GIF and SVG images', function () {
@@ -32,53 +34,62 @@ module.exports = grunt => {
 			interlaced: true,
 			optimizationLevel: 3,
 			progressive: true,
-			concurrency: os.cpus().length
+			concurrency: os.cpus().length,
 		});
 
 		if (Array.isArray(options.svgoPlugins)) {
 			options.plugins = options.svgoPlugins;
 		}
 
-		const plugins = options.use || getDefaultPlugins(grunt, options);
+		const pluginsPromise = options.use
+			? Promise.resolve(options.use)
+			: getDefaultPlugins(grunt, options);
+		/* eslint-disable node/no-unsupported-features/es-syntax */
+		const imageminPromise = import('imagemin')
+			.then(imagemin => imagemin.default);
+		const pMapPromise = import('p-map')
+			.then(pMap => pMap.default);
+		/* eslint-enable node/no-unsupported-features/es-syntax */
 
-		let totalBytes = 0;
-		let totalSavedBytes = 0;
-		let totalFiles = 0;
+		Promise.all([pluginsPromise, imageminPromise, pMapPromise]).then(([plugins, imagemin, pMap]) => {
+			let totalBytes = 0;
+			let totalSavedBytes = 0;
+			let totalFiles = 0;
 
-		const processFile = file => Promise.resolve(grunt.file.read(file.src[0], {encoding: null}))
-			.then(buf => Promise.all([imagemin.buffer(buf, {plugins}), buf]))
-			.then(res => {
-				const [optimizedBuf, originalBuf] = res;
-				const originalSize = originalBuf.length;
-				const optimizedSize = optimizedBuf.length;
-				const saved = originalSize - optimizedSize;
-				const percent = originalSize > 0 ? (saved / originalSize) * 100 : 0;
-				const savedMsg = `saved ${prettyBytes(saved)} - ${percent.toFixed(1).replace(/\.0$/, '')}%`;
-				const msg = saved > 0 ? savedMsg : 'already optimized';
+			const processFile = file => Promise.resolve(grunt.file.read(file.src[0], {encoding: null}))
+				.then(buf => Promise.all([imagemin.buffer(buf, {plugins}), buf]))
+				.then(([optimizedBuf, originalBuf]) => {
+					const originalSize = originalBuf.length;
+					const optimizedSize = optimizedBuf.length;
+					const saved = originalSize - optimizedSize;
+					const percent = originalSize > 0 ? (saved / originalSize) * 100 : 0;
+					const savedMessage = `saved ${prettyBytes(saved)} - ${percent.toFixed(1).replace(/\.0$/, '')}%`;
+					const message = saved > 0 ? savedMessage : 'already optimized';
 
-				if (saved > 0) {
-					totalBytes += originalSize;
-					totalSavedBytes += saved;
-					totalFiles++;
+					if (saved > 0) {
+						totalBytes += originalSize;
+						totalSavedBytes += saved;
+						totalFiles++;
+					}
+
+					grunt.file.write(file.dest, optimizedBuf);
+					grunt.verbose.writeln(chalk.green('✔ ') + file.src[0] + chalk.gray(` (${message})`));
+				})
+				.catch(error => {
+					grunt.warn(`${error} in file ${file.src[0]}`);
+				});
+
+			pMap(this.files, processFile, {concurrency: options.concurrency}).then(() => {
+				const percent = totalBytes > 0 ? (totalSavedBytes / totalBytes) * 100 : 0;
+				let message = `Minified ${totalFiles} ${plur('image', totalFiles)}`;
+
+				if (totalFiles > 0) {
+					message += chalk.gray(` (saved ${prettyBytes(totalSavedBytes)} - ${percent.toFixed(1).replace(/\.0$/, '')}%)`);
 				}
 
-				grunt.file.write(file.dest, optimizedBuf);
-				grunt.verbose.writeln(chalk.green('✔ ') + file.src[0] + chalk.gray(` (${msg})`));
-			})
-			.catch(error => {
-				grunt.warn(`${error} in file ${file.src[0]}`);
+				grunt.log.writeln(message);
+				done();
 			});
-
-		pMap(this.files, processFile, {concurrency: options.concurrency}).then(() => {
-			const percent = totalBytes > 0 ? (totalSavedBytes / totalBytes) * 100 : 0;
-			let msg = `Minified ${totalFiles} ${plur('image', totalFiles)}`;
-
-			if (totalFiles > 0) {
-				msg += chalk.gray(` (saved ${prettyBytes(totalSavedBytes)} - ${percent.toFixed(1).replace(/\.0$/, '')}%)`);
-			}
-
-			grunt.log.writeln(msg);
-			done();
 		});
 	});
 };
